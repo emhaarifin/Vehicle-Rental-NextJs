f4391b2a6cd034f9c67b36cc0cb19f011e4de8d2
Alan Nugraha <101muhammadarifin@gmail.com>
2021-08-16 02:57:37 +0700

    testing card product

--- pages/vehicle-type.js
+++ pages/vehicle-type.js
@@ -1,9 +1,23 @@
+import CardProduct from '../components/molecules/CardProduct';
 import Main from '../components/templates/Main';
+import { facebook } from '../public/asset';
 
 function VehicleType() {
   return (
     <Main>
       <p>vehicle type</p>
+      <div style={{ display: 'flex', flexWrap: 'wrap', gap: '1.7rem' }}>
+        <CardProduct />
+        <CardProduct />
+        <CardProduct />
+        <CardProduct />
+        <CardProduct />
+        <CardProduct />
+        <CardProduct />
+        <CardProduct />
+        <CardProduct />
+        <CardProduct />
+      </div>
     </Main>
   );
 }
