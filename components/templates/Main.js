a282bc1aa318bbf7001b6e96be635d6f46ac5f50
Alan Nugraha <101muhammadarifin@gmail.com>
2021-08-17 21:39:59 +0700

    feat: setting responsive

--- components/templates/Main.js
+++ components/templates/Main.js
@@ -1,14 +1,24 @@
 import Footer from '../organism/Footer';
 import Navbar from '../organism/Navbar';
+import styled from 'styled-components';
+import { customMedia } from '../../styles/breakpoint';
 
 function Main({ children }) {
   return (
     <>
       <Navbar />
-      {children}
+      <ContainerMain>{children}</ContainerMain>
       <Footer />
     </>
   );
 }
 
 export default Main;
+
+const ContainerMain = styled.div`
+  padding: 5rem;
+  margin: 0 auto;
+  ${customMedia.lessThan('media_sm')`
+  padding: 1rem;
+`}
+`;
