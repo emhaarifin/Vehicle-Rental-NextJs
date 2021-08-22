d67e63becb6f6cc78903014fe35aaa504673fa0b
Alan Nugraha <101muhammadarifin@gmail.com>
2021-08-22 17:01:25 +0700

    update css

--- components/molecules/CardContainer.js
+++ components/molecules/CardContainer.js
@@ -9,23 +9,6 @@ export default CardContainer;
 
 const ContainerCard = styled.div`
   display: flex;
-  flex-grow: 1;
   flex-wrap: wrap;
-  ${customMedia.lessThan('media_sm')`
-  gap: 0rem;
-`}
-  // ${customMedia.lessThan('media_sm')`
-//   gap: 0rem;
-// `}
-  ${customMedia.greaterThan('media_lg')`
-  gap: 3rem;
-`}
-  ${customMedia.greaterThan('media_xl')`
-  // justify-content: space-between
-  // gap: 4.5rem;
-  gap: 4.91rem;
-`}
-${customMedia.greaterThan('1800px')`
-ju
-`}
+  gap: 2%;
 `;
