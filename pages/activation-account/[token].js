e059ce06b5c25b36cb038c3ea6e34a70bd0958b4
emhaarifin <101muhammadarifin@gmail.com>
2021-09-15 02:38:25 +0700

    update

--- pages/activation-account/[token].js
+++ pages/activation-account/[token].js
@@ -1,16 +1,9 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 /* eslint-disable react-hooks/exhaustive-deps */
 import swal from 'sweetalert';
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
+import Router from 'next/router';
 import { axios, publicRoute } from '@/configs';
 function activation() {
-  // const Success = async () => {
-  // };
-  // useEffect(() => {
-  //   Success();
-  // }, []);
-
   return <></>;
 }
 
@@ -20,10 +13,15 @@ export const getServerSideProps = publicRoute(async (context) => {
   const token = context.params.token;
   axios
     .get(`/auth/activation/${token}`)
-    .then(async () => {
+    .then(async (result) => {
+      console.log(result, ' ini res');
       await swal('Success', 'Suskes Verifikasi Email', 'success');
+      context.res.writeHead(307, {
+        Location: `https://vehicle-rental-next-js.vercel.app/`,
+      });
     })
-    .catch(async () => {
+    .catch(async (error) => {
+      console.log(error, 'ini eror');
       await swal('Opss...', 'eror', 'error');
     });
   return {
