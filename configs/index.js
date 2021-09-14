22932adb37fc3f4b6ed1b4dc9a49453f8575f2c0
emhaarifin <101muhammadarifin@gmail.com>
2021-09-14 09:45:56 +0700

    feat: fix absolute path:

--- configs/index.js
+++ configs/index.js
@@ -1,7 +1,8 @@
 import axios from './axios';
-import userAction from './redux/actions/user';
-import reservationAction from './redux/actions/reservation';
+import { Logout, updateProfile } from './redux/actions/user';
+import { addReservation } from './redux/actions/reservation';
+
 import { publicRoute } from './routes/publicRoute';
 import { privateRoute } from './routes/privateRoute';
 import { privateRouteAdmin } from './routes/privateRouteAdmin';
-export { axios, userAction, reservationAction, publicRoute, privateRoute, privateRouteAdmin };
+export { axios, Logout, updateProfile, addReservation, publicRoute, privateRoute, privateRouteAdmin };
